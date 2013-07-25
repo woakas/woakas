d85631e035413916f19b53b17d513e3e4f35e54e
Gustavo Andrés Angulo <woakas@gmail.com>
2013-07-25 02:21:33 -0500

    Added some features to this blog

--- README.rst
+++ README.rst
@@ -2,3 +2,26 @@ Woakas Blog
 ===========
 
 Sources for http://woakas.pehik.co
+
+
+Compile and Install
+-------------------
+
+Install dependences::
+
+    pip install -r requirements.pip
+
+
+Initialize submodules for install pelican-plugins in the root path repository::
+
+    git submodule init
+    git submodule update
+
+
+then compile::
+
+    cd site; make html
+
+Preview the output in http://localhost:8000/::
+
+    cd output; python -m SimpleHTTPServer
--- site/pelicanconf.py
+++ site/pelicanconf.py
@@ -24,16 +24,38 @@ FEED_DOMAIN = SITEURL
 DISPLAY_PAGES_ON_MENU = True
 
 
+# Plugins
+PLUGIN_PATH = '../pelican-plugins'
+PLUGINS = ['assets', 'sitemap', 'gravatar']
+
+SITEMAP = {
+    'format': 'xml',
+    'priorities': {
+        'articles': 0.5,
+        'indexes': 0.5,
+        'pages': 0.5
+    },
+    'changefreqs': {
+        'articles': 'monthly',
+        'indexes': 'daily',
+        'pages': 'monthly'
+    }
+}
+
+# Static files
 FILES_TO_COPY = (('extra/robots.txt', 'robots.txt'),)
 STATIC_PATHS = ["static", ]
 
 FEED_RSS = 'feeds/all.rss.xml'
 CATEGORY_FEED_RSS = 'feeds/%s.rss.xml'
 
+# fotter colophon
 COLOPHON = True
 COLOPHON_TITLE = 'Colophon'
 COLOPHON_CONTENT = 'El d&iacute;a es largo y la vida corta'
 
+# Theme
 THEME = "../theme-built-texts"
 
+# Analytics
 MIXPANEL_ANALYTICS = "66043383c2f73a32095329de656bc0fc"
